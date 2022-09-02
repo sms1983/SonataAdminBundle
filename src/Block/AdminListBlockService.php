f91221abbc68f8fde3611c8e5e77ff377c46e1ca
Yan Bein <rbeinyan@gmail.com>
2022-09-02 10:12:41 -0300

    Fix

--- src/Block/AdminListBlockService.php
+++ src/Block/AdminListBlockService.php
@@ -41,7 +41,7 @@ final class AdminListBlockService extends AbstractBlockService
         $this->templateRegistry = $templateRegistry;
     }
 
-    public function execute(BlockContextInterface $blockContext, ?Response $response = null): Response: Response
+    public function execute(BlockContextInterface $blockContext, ?Response $response = null): Response
     {
         $dashboardGroups = $this->pool->getDashboardGroups();
 
